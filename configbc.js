474926d6ebf3b7d9ce1794a37052a384461afe8b
DeathGM <51689465+DeathGM@users.noreply.github.com>
2022-08-12 20:57:29 +0700

    1

--- configbc.js
+++ configbc.js
@@ -4,20 +4,20 @@
  let config = {
    room1: {
      autoStart: true,
-     roomName: `🏆 Thế Vận Hội 1 - Bóng Chuyền 🏆`,
+     roomName: `🏆 Thế Vận Hội 1 - Bóng Chuyền - 🏐 🏆`,
      noPlayer: true,
      maxPlayers: 13,
      public: true,
-     geo: {code: "VN", lat: 21.02888, lon: 105.85464},
+     geo: {code: "VN", lat: 21.02886, lon: 105.85464},
      roomScript: "/home/hqd_wendal/haxball-volleyball/tvh1.js",
    },
    room2: {
     autoStart: true,
-    roomName: `🏆 Thế Vận Hội 2 - Bóng Chuyền🏆`,
+    roomName: `🏆 Thế Vận Hội 2 - Bóng Chuyền - 🏐 🏆`,
     noPlayer: true,
     maxPlayers: 13,
     public: true,
-    geo: {code: "VN", lat: 21.02888, lon: 105.85464},
+    geo: {code: "VN", lat: 21.02886, lon: 105.85464},
     roomScript: "/home/hqd_wendal/haxball-volleyball/tvh1.js",
   },
 
